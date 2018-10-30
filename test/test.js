2b5fb7a94514b821ecc41c521371b117735668e8
Abhinay <abhinayreddy1278@gmail.com>
2018-10-29 21:59:03 -0500

    commit

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
 QUnit.test('Testing calculateArea function with several sets of inputs', function (assert) {
-    assert.equal(calculate(), 25, 'Tested with two relatively small positive numbers');
+    assert.equal(calculate(), 25, 'Tested with two relatively small positive  numbers');
+
 }
